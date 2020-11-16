604d21bb8062d8378b50175a1db830a6b2a7cb1c
Myles <mylesjanik@gmail.com>
2020-11-15 20:56:31 -0800

    commit tree 05a587abdd8f0f8b97d87c2fd4960b86bc0b8067

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
+class CashRegister
+    attr_reader :total
 
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity = 0)
+
+    end
+end
